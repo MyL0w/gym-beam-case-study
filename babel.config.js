26d5b92548a2908853799a9e90819c2593634898
martinsojcak <sojcakmartin@gmail.com>
2025-05-13 09:12:01 +0200

    Initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ['module-resolver', {
+      'alias': {
+        '@screens': './src/screens',
+        '@firebase': './src/firebase',
+        '@languages': './src/assets/languages',
+        '@assets': './src/assets',
+        '@navigation': './src/navigation',
+        '@modules': './src/modules',
+        '@components': './src/components',
+        '@hooks': './src/lib/hooks',
+        '@constants': './src/lib/constants',
+        '@services': './src/lib/services',
+        '@context': './src/lib/context',
+        '@helpers': './src/lib/helpers',
+      },
+    }],
+  ],
 };
